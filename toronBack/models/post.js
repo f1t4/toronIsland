71489ea9ad3290d77a76aa0e51d3a5832c0b6e9f
8lueis <hkahk@naver.com>
2023-12-30 17:23:07 +0900

    khk 1230 AgreeContainer 수정

--- toronBack/models/post.js
+++ toronBack/models/post.js
@@ -2,17 +2,18 @@
 // init -> mysql.createConnection db랑 연결
 // db_toron -> 연결 정보 전달  
 // connect -> 연결 객체 전달 받음 
-const mysqlModule = require('../config/db.js');
+const mysqlModule = require('../config/db');
+// 말 그대로 내 DB랑 연결하고 있음 연결 초기화 같은 건가 
 const connection = mysqlModule.init();
 
 const express = require('express');
 const app = express();
 
+// sql에 쿼리문 전송 
 connection.query('SELECT * FROM board', (err, result)=> {
     if(err) throw err;
     console.log(result);
 });
 
-
-// 오류 개빡친다 
+// 위의 쿼리문 로직 내 내용을 전송? 
 mysqlModule.connect(connection);
